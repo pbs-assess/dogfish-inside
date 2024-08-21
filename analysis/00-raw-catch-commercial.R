770fe805c71d90526d707498473dd2bcdace0bf1
Luke Rogers <luke.allister.rogers@gmail.com>
2024-08-21 16:06:57 -0700

    separated scripts by data file

--- analysis/00-raw-catch-commercial.R
+++ analysis/00-raw-catch-commercial.R
@@ -1,8 +1,7 @@
 # remotes::install_github("pbs-assess/gfdata")
 library(gfdata)
-library(readr)
 
-# Read contemporary ------------------------------------------------------------
+# Read data --------------------------------------------------------------------
 
 d <- gfdata::get_catch(
   species = "044",
@@ -28,36 +27,3 @@ d <- gfdata::get_catch(
 tibble::view(d)
 # Write catch
 saveRDS(d, file = "data/raw/catch-commercial.rds")
-
-# Read historical --------------------------------------------------------------
-
-# From https://github.com/pbs-assess/dogfish-assess/
-path <- file.path(dirname(getwd()), "dogfish-assess/data/raw/")
-
-
-# All 1935-1965
-# Galluci et al. (2011) ResDoc 2011/034 Table 2 (p.33)
-# Annual landings (tonnes)
-
-d <- read_csv(paste0(path, "catches-all-gears-1935-1965.csv"), skip = 2)
-
-# Write
-saveRDS(d, "data/raw/catch-commercial-all-gears-1935-1965.rds")
-
-# Longline 1966-2008
-# Galluci et al. (2011) ResDoc 2011/034 Table 3 (p.34)
-# Annual landings (tonnes)
-
-d <- read_csv(paste0(path, "catches-longline-1966-2008.csv"), skip = 2)
-
-# Write
-saveRDS(d, "data/raw/catch-commercial-longline-1966-2008.rds")
-
-# Trawl 1966-2008
-# Galluci et al. (2011) ResDoc 2011/034 Table 3 (p.34)
-# Annual landings (tonnes)
-
-d <- read_csv(paste0(path, "catches-trawl-1966-2008.csv"), skip = 2)
-
-# Write
-saveRDS(d, "data/raw/catch-commercial-trawl-1966-2008.rds")
