7f80220176f85646d24e9e1c3873a9af8d0cb01d
Luke Rogers <luke.allister.rogers@gmail.com>
2024-08-29 16:01:14 -0700

    updated recreational data

--- analysis/01-gen-catch-recreational-creel.R
+++ analysis/01-gen-catch-recreational-creel.R
@@ -1,10 +1,43 @@
+# Load
+library(dplyr)
+library(gfplot)
+library(ggplot2)
+library(tibble)
+
 # Read data --------------------------------------------------------------------
 
-d <- readRDS("data/raw/catch-recreational-creel.rds")
-
-
-
-
+d <- readRDS("data/raw/catch-recreational-creel.rds") |>
+  dplyr::mutate(
+    species_common_name = tolower(species),
+    area = dplyr::case_match(pfma, paste0("PFMA ", c(12:20, 28:29)) ~ "4B"),
+    gear = "Recreational",
+    type = dplyr::case_match(disposition, "Kept" ~ "landings"),
+    catch_pc = estimate,
+    variance = standard_error^2,
+    source = "Creel"
+  ) |>
+  dplyr::group_by(year, species_common_name, area, gear, type, source) |>
+  dplyr::summarise(
+    catch_pc = sum(catch_pc, na.rm = TRUE),
+    variance = sum(variance, na.rm = TRUE), # Assumes catch independent
+    .groups = "drop"
+  ) |>
+  dplyr::mutate(
+    catch_kpc = 1e-03 * catch_pc,
+    variance = (variance + catch_pc^2) * (1e-03)^2 - (catch_pc^2) * (1e-03)^2,
+    se = sqrt(variance)
+  ) |>
+  dplyr::select(
+    year,
+    species_common_name,
+    area,
+    gear,
+    type,
+    catch_kpc,
+    se,
+    source
+  ) |>
+  tidyr::drop_na()
 
 # Write data -------------------------------------------------------------------
 
--- analysis/01-gen-catch-recreational-irec.R
+++ analysis/01-gen-catch-recreational-irec.R
@@ -1,9 +1,48 @@
+# Load
+library(dplyr)
+library(gfplot)
+library(ggplot2)
+library(tibble)
+
 # Read data --------------------------------------------------------------------
 
-d <- readRDS("data/raw/catch-recreational-irec.rds")
+d <- readRDS("data/raw/catch-recreational-irec.rds") |>
+  dplyr::filter(disposition == "Kept") |>
+  # TODO update from here
+  # dplyr::mutate(
+  #   species_common_name = "north pacific spiny dogfish",
+  #   area = dplyr::case_match(pfma, paste0("PFMA ", c(12:20, 28:29)) ~ "4B"),
+  #   gear = "Recreational",
+  #   type = dplyr::case_match(disposition, "Kept" ~ "landings"),
+  #   catch_pc = estimate,
+  #   variance = standard_error^2,
+  #   source = "Creel"
+  # ) |>
+  # dplyr::group_by(year, species_common_name, area, gear, type, source) |>
+  # dplyr::summarise(
+  #   catch_pc = sum(catch_pc, na.rm = TRUE),
+  #   variance = sum(variance, na.rm = TRUE), # Assumes catch independent
+  #   .groups = "drop"
+  # ) |>
+  # dplyr::mutate(
+  #   catch_kpc = 1e-03 * catch_pc,
+  #   variance = (variance + catch_pc^2) * (1e-03)^2 - (catch_pc^2) * (1e-03)^2,
+  #   se = sqrt(variance)
+  # ) |>
+  # dplyr::select(
+  #   year,
+  #   species_common_name,
+  #   area,
+  #   gear,
+  #   type,
+  #   catch_kpc,
+  #   se,
+  #   source
+  # ) |>
+  tidyr::drop_na()
 
 
 
 # Write data -------------------------------------------------------------------
 
-saveRDS(d, file = "data/generated/catch-recreational-irec.rds")
+# saveRDS(d, file = "data/generated/catch-recreational-irec.rds")
