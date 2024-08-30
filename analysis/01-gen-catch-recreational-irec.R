a8de4b66b24303d9313b8511ad39ae0d20b938f1
Luke Rogers <luke.allister.rogers@gmail.com>
2024-08-30 13:34:50 -0700

    updated recreational irec data

--- analysis/01-gen-catch-recreational-irec.R
+++ analysis/01-gen-catch-recreational-irec.R
@@ -8,41 +8,46 @@ library(tibble)
 
 d <- readRDS("data/raw/catch-recreational-irec.rds") |>
   dplyr::filter(disposition == "Kept") |>
-  # TODO update from here
-  # dplyr::mutate(
-  #   species_common_name = "north pacific spiny dogfish",
-  #   area = dplyr::case_match(pfma, paste0("PFMA ", c(12:20, 28:29)) ~ "4B"),
-  #   gear = "Recreational",
-  #   type = dplyr::case_match(disposition, "Kept" ~ "landings"),
-  #   catch_pc = estimate,
-  #   variance = standard_error^2,
-  #   source = "Creel"
-  # ) |>
-  # dplyr::group_by(year, species_common_name, area, gear, type, source) |>
-  # dplyr::summarise(
-  #   catch_pc = sum(catch_pc, na.rm = TRUE),
-  #   variance = sum(variance, na.rm = TRUE), # Assumes catch independent
-  #   .groups = "drop"
-  # ) |>
-  # dplyr::mutate(
-  #   catch_kpc = 1e-03 * catch_pc,
-  #   variance = (variance + catch_pc^2) * (1e-03)^2 - (catch_pc^2) * (1e-03)^2,
-  #   se = sqrt(variance)
-  # ) |>
-  # dplyr::select(
-  #   year,
-  #   species_common_name,
-  #   area,
-  #   gear,
-  #   type,
-  #   catch_kpc,
-  #   se,
-  #   source
-  # ) |>
+  dplyr::mutate(
+    species_common_name = dplyr::case_match(
+      item,
+      "Dogfish" ~ "north pacific spiny dogfish",
+    ),
+    area = dplyr::case_match(
+      area,
+      paste0("Area ", c(12:20, 28:29))           ~ "4B",
+      paste0("Area 19", c(" (GS)", " (JDF)"))    ~ "4B",
+      paste0("Area 20", c(" (East)", " (West)")) ~ "4B",
+      c("Area 29 (Marine)")                      ~ "4B"
+    ),
+    gear = "Recreational",
+    type = dplyr::case_match(disposition, "Kept" ~ "landings"),
+    catch_pc = estimate,
+    source = "iREC"
+  ) |>
+  dplyr::group_by(year, species_common_name, area, gear, type, source) |>
+  dplyr::summarise(
+    catch_pc = sum(catch_pc, na.rm = TRUE),
+    variance = sum(variance, na.rm = TRUE), # Assumes catch independent
+    .groups = "drop"
+  ) |>
+  dplyr::mutate(
+    catch_kpc = 1e-03 * catch_pc,
+    variance = (variance + catch_pc^2) * (1e-03)^2 - (catch_pc^2) * (1e-03)^2,
+    se = sqrt(variance)
+  ) |>
+  dplyr::select(
+    year,
+    species_common_name,
+    area,
+    gear,
+    type,
+    catch_kpc,
+    se,
+    source
+  ) |>
   tidyr::drop_na()
-
-
 
 # Write data -------------------------------------------------------------------
 
-# saveRDS(d, file = "data/generated/catch-recreational-irec.rds")
+saveRDS(d, file = "data/generated/catch-recreational-irec.rds")
