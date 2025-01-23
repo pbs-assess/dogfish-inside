21f6e453a3ea97d0a63fb5ad84e6c2a8ab152bb4
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2025-01-22 17:32:22 -0800

    data pulls

--- analysis/data-prep/000-table-of-Contents.R
+++ analysis/data-prep/000-table-of-Contents.R
@@ -1,1 +1,6 @@
+#TOC to keep up clear on what script does what
+
+01-pull-gfdata.R #pulls in the survey sets, samples, and commercial data, also removes data with privacy concerns
+01a-collate-commercial-landings-discards.R #this is pulling in the estimate commercial data prior to 2000/2006 and converting  to .rds
+00-raw-samples-survey.R #likely get rid of this
 
