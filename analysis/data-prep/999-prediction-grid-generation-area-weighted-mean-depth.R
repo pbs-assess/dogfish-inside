3e45f7a4d928486762c6833030c1f6ebefdaed2e
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2025-02-19 15:13:25 -0800

    init commit

---
+++ analysis/data-prep/999-prediction-grid-generation-area-weighted-mean-depth.R
@@ -0,0 +1,143 @@
+# remotes::install_github("pbs-assess/gfplot")
+library(gfplot)
+library(gfdata)
+library(terra)
+library(tidyverse)
+library(sf)
+
+# NOTES
+# Generate a prediction grid with an area-weighted mean depth (versus centroid depth) and trim cell area to coastline (i.e. overwater area).
+# make grids that covers HBLL N and S and all of Strait of Georgia
+# have the HBLL INS grid nested inside the bigger grid
+
+# DEM and land mass data is on one drive (too big for github): OneDrive - DFO-MPO\lingcod and dogfish program\dogfish-inside\data-raw\
+# Could get a higher resolution DEM
+# DEM data from here: https://osdp-psdo.canada.ca/dp/en/search/metadata/NRCAN-FGP-1-e6e11b99-f0cc-44f7-f5eb-3b995fb1637e
+# BC land mass data from here: https://catalogue.data.gov.bc.ca/dataset/province-of-british-columbia-boundary-terrestrial/resource/3d72cf36-ab53-4a2a-9988-a883d7488384
+
+# Jillian created grids for HBLL N and S that have over water area and area weighted depth. See here: https://github.com/pbs-assess/gfdata/blob/85f4355ab7cba133674f425e3892f80e74894dfa/data-raw/survey_blocks.R#L80
+
+# params
+bccrs <- 32609
+
+
+# load data
+depth <- terra::rast("C:/Users/DAVIDSONLI/OneDrive - DFO-MPO/lingcod and dogfish program/dogfish-inside/data-raw/dem_excluded_land.tif")
+st_layers("C:/Users/DAVIDSONLI/OneDrive - DFO-MPO/lingcod and dogfish program/dogfish-inside/data-raw/BC_Boundary_Terrestrial.gpkg") # get layer name of single polygon
+bc_poly <- st_read("C:/Users/DAVIDSONLI/OneDrive - DFO-MPO/lingcod and dogfish program/dogfish-inside/data-raw/BC_Boundary_Terrestrial.gpkg", layer = "BC_Boundary_Terrestrial_Multipart")
+bc_shelf_poly <- st_read("C:/Users/DAVIDSONLI/OneDrive - DFO-MPO/lingcod and dogfish program/dogfish-inside/data-raw/Shelf_polygon.shp") # something like this probably this file on pacea I used this instead of trimming to the bc_poly as this has the boundary of the SOG
+
+
+# create a grid that covers all of the HBLL INS S, S and the rest of the SOG
+# make sure it's only overwater area
+# make sure the depth is area weighted mean depth using the DEM
+depthcr1 <- crop(depth, c(800000, 1313158, 304657.4, 700000))
+r.pr <- terra::project(depthcr1, "EPSG:32609", res = 2000)
+plot(r.pr)
+
+
+bc_shelf_polypr <- bc_shelf_poly |>
+  st_transform(32609) # sog polygon projected
+
+# small_bc_ins <- bc_poly |>
+#   st_transform(32609) |>
+#   st_crop(r.pr)
+# plot(st_geometry(small_bc_ins))
+
+
+# create a grid that covers SOG with HBLL INS S and N nested in this bigger grid
+blocks <- gfdata::survey_blocks |> filter(survey_abbrev %in% c("HBLL INS S", "HBLL INS N")) # area is overwater area
+blocks <- blocks |> rename(area_orig = area)
+ggplot() +
+  geom_sf(data = blocks, aes(fill = depth_m))
+
+
+# polygon of SOG
+grid <- blocks |> sf::st_make_grid(square = TRUE, cellsize = 2000)
+grid |>
+  ggplot() +
+  geom_sf() +
+  geom_sf(data = blocks, fill = "red")
+
+
+# make a polygon around HBLL N and S grid
+hulls <- st_buffer(blocks, dist = 20000)
+soghull <- spatialEco::sf_dissolve(hulls) |> st_as_sf()
+ggplot() +
+  geom_sf(data = soghull) +
+  geom_sf(data = blocks)
+
+soggrid <- st_intersection(soghull, grid)
+soggrid2 <- st_intersection(soggrid, bc_shelf_polypr)
+
+ggplot() +
+  geom_sf(data = soggrid2) +
+  geom_sf(data = blocks, fill = "red")
+
+soggrid3 <- st_join(soggrid2, blocks) # get attributes 
+
+soggrid3 <- soggrid3 %>%
+  mutate(area_km2 = st_area(., units = "km^2")/1000000) |>
+  mutate(FID = seq(1:n())) |> 
+  mutate(area_km2 = as.numeric(area_km2))
+  
+ggplot() +
+  geom_sf(data = soggrid3, aes(fill = survey_abbrev))
+
+soggrid3 |> 
+  drop_na(area_orig) |> 
+  ggplot() +
+  geom_point(aes(area_orig, area_km2)) #differences between Jillian's overwater calc (area_orig) and what's completed here. 
+
+
+# intersect SOG grid with the DEM to get depth
+bdepths <- crop(r.pr, soggrid3)
+bdepths_poly <- terra::as.polygons(bdepths)
+bdepths_sf <- st_as_sf(bdepths_poly)
+
+ggplot() +
+  geom_sf(data = (bdepths_sf), colour = "red") + 
+  geom_sf(data = (soggrid3), aes(colour = FID))
+
+bdepths_int <- st_intersection(soggrid3, bdepths_sf) %>%
+  mutate(area_int = st_area(., units = "km^2")/1000000) |> 
+  mutate(area_int = as.numeric(area_int)) #not all grid cells intersect with the DEM
+
+ggplot() +
+   geom_sf(data = soggrid3, colour = "red") + 
+   geom_sf(data = bdepths_int, aes(colour = west_coast_dem))
+
+bdepths2 <- bdepths_int |>
+  mutate(weight = area_int/area_km2) |> 
+  mutate(area_weighted_depth = (west_coast_dem*weight)) |>
+  group_by(FID) |>
+  mutate(area_weighted_mean_depth = sum(area_weighted_depth)/sum(weight)) |>
+  distinct(FID, .keep_all = TRUE) 
+
+bdepths3 <- bdepths2 |> 
+  st_centroid () |> 
+  mutate(
+    UTM.lat.m = unlist(purrr::map(x, 2)),
+    UTM.lon.m = unlist(purrr::map(x, 1))
+  ) |>
+  st_drop_geometry() |> 
+  mutate(UTM.lat = UTM.lat.m/1000, UTM.lon = UTM.lon.m/1000) |> 
+  mutate(weighted_depth_m = area_weighted_mean_depth * -1)
+
+x <- dplyr::select(bdepths3, FID, area_weighted_mean_depth, weighted_depth_m, UTM.lat.m, UTM.lat, UTM.lon, UTM.lon.m)
+y <- soggrid3 |> st_drop_geometry()
+soggrid4 <- left_join(y, x)
+
+ggplot() +
+  geom_sf(data = bdepths_int)
+ggplot() +
+  geom_point(data = soggrid4, aes(UTM.lon, UTM.lat, colour = area_weighted_mean_depth)) #for the point missing a rea weighted mean depth use the depth column from the original blocks dataset? or keep NA?
+ggplot() +
+  geom_point(data = soggrid4, aes(depth_m, weighted_depth_m, colour = area_weighted_mean_depth)) #made things worse?? IE deeper??
+
+range(na.omit(soggrid4$depth_m))
+range(na.omit(soggrid4$weighted_depth_m))
+range(na.omit(soggrid4$area_km2))
+
+saveRDS(grid, "output/prediction-grid-hbll-n-s.rds")
+
